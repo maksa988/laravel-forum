4a199ca205723f4d2f05c18685aece1edc0df1fc
maksa988 <maksa988ua@gmail.com>
2019-01-18 13:49:37 +0200

    Create Thread and Reply models

--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -13,12 +13,14 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\Models\User::class, function (Faker $faker) {
+$factory->define(App\Models\Reply::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'user_id' => function() {
+            return factory('App\Models\User')->create()->id;
+        },
+        'thread_id' => function() {
+            return factory('App\Models\Thread')->create()->id;
+        },
+        'body' => $faker->paragraph,
     ];
 });
--- database/factories/ThreadFactory.php
+++ database/factories/ThreadFactory.php
@@ -13,12 +13,12 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\Models\User::class, function (Faker $faker) {
+$factory->define(App\Models\Thread::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'user_id' => function() {
+            return factory('App\Models\User')->create()->id;
+        },
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph,
     ];
 });
