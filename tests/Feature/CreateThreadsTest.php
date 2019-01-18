5b5c6526c7f40c39e3372935057da0611bf2520c
maksa988 <maksa988ua@gmail.com>
2019-01-18 19:02:51 +0200

    Create new threads test

---
+++ tests/Feature/CreateThreadsTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Thread;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class CreateThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function guests_may_not_create_threads()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        $thread = factory(Thread::class)->make();
+
+        $this->post('/threads', $thread->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
+    }
+
+    /** @test */
+    public function an_authenticated_user_can_create_new_forum_threads()
+    {
+        $this->actingAs(factory(User::class)->create());
+
+        $thread = factory(Thread::class)->make();
+
+        $this->post('/threads', $thread->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
+    }
+}
