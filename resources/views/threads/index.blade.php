08bc9d35d1511c86b23693429cb83291e83be271
maksa988 <maksa988ua@gmail.com>
2019-01-19 01:13:33 +0200

    Thread replies count

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -10,9 +10,17 @@
                     <div class="card-body">
                         @foreach($threads as $thread)
                             <article>
-                                <h4>
-                                    <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
-                                </h4>
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <h4>
+                                            <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
+                                        </h4>
+                                    </div>
+                                    <div class="col-lg-6 text-right">
+                                        <a href="{{ $thread->path() }}">{{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}</a>
+                                    </div>
+                                </div>
+
                                 <div class="body">{{ $thread->body }}</div>
                             </article>
 
