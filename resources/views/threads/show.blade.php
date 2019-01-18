23cd12bfb424c86fd10a13f10c5a73aed8ee60f2
maksa988 <maksa988ua@gmail.com>
2019-01-18 14:41:28 +0200

    Replies

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -13,5 +13,22 @@
                 </div>
             </div>
         </div>
+
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                @foreach($thread->replies as $reply)
+                <div class="card mt-3">
+                    <div class="card-header">
+                        <a href="#">{{ $reply->owner->name }}</a> said {{ $reply->created_at->diffForHumans() }}...
+                    </div>
+
+                    <div class="card-body">
+                        {{ $reply->body }}
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+
     </div>
 @endsection
